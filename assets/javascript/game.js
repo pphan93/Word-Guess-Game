5d5615f2123da86c85a88f18d2e391f52dcef493
Phuoc Phan <phuocphan12@gmail.com>
2019-01-26 23:50:39 -0500

    take k from key press

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,6 +7,7 @@ var lettersGuessed = [];
 var textOutput;
 var failMessage;
 var correctMessage;
+var inputLetter
 //var gameOver = false;
 
 failMessage = "YOU FAILED!";
@@ -15,6 +16,7 @@ correctMessage = "YOU WIN";
 
 var i = 0
 
+//guessWord = guessWord.toUpperCase();
 
 console.log(wordLength);
 
@@ -24,50 +26,65 @@ initGame();
 
 console.log("testing: " + lettersLeft);
 
-while (!gameOver(guessesLeft,lettersLeft)){
-    var inputLetter = prompt("Type in a letter to guess: ");
-    textOutput = guessCheck(inputLetter);
-    document.getElementById("wordBox").innerHTML += textOutput;
-}
 
+document.body.onkeypress = function (event) {
 
-function resultOuput (){
-    if (guessesLeft === 0){
-        document.getElementById("status").innerHTML += failMessage;
-    }
-
-    else if(lettersLeft === 0){
-        document.getElementById("status").innerHTML += correctMessage;
-        document.getElementById("wordBox").innerHTML += textOutput;
-    }
-}
-
-
-function gameOver(guessesLeft,lettersLeft){
-    if (guessesLeft === 0 || lettersLeft === 0) {
+    if (guessesLeft !== 0 && lettersLeft !== 0) {
+        inputLetter = String.fromCharCode(event.keyCode);
+        document.getElementById("kp").innerHTML = inputLetter;
+        textOutput = guessCheck(inputLetter);
+        document.getElementById("wordBox").innerHTML = textOutput;
+        //var inputLetter = prompt("Type in a letter to guess: ");
         resultOuput();
+    }
+
+}
+
+
+//while (!gameOver(guessesLeft,lettersLeft)){
+//var inputLetter = prompt("Type in a letter to guess: ");
+//   textOutput = guessCheck(inputLetter);
+//   document.getElementById("wordBox").innerHTML += textOutput;
+//}
+
+
+function resultOuput() {
+    if (guessesLeft === 0) {
+        return document.getElementById("status").innerHTML = failMessage;
+    }
+
+    else if (lettersLeft === 0) {
+        return document.getElementById("status").innerHTML = correctMessage;
+        //document.getElementById("wordBox").innerHTML = textOutput;
+    }
+}
+
+
+function gameOver(guessesLeft, lettersLeft) {
+    if (guessesLeft === 0 || lettersLeft === 0) {
         return true;
     }
-    
+
 }
 
 
-function initGame(){
-    for   (i; i < wordLength; i++){
+function initGame() {
+    for (i; i < wordLength; i++) {
         guessedRight[i] = '_';
     }
 }
 
 
-function guessCheck(inputLetter){
+function guessCheck(inputLetter) {
+    console.log("guessCHeckInput: " + inputLetter);
     var foundPosition = guessWord.indexOf(inputLetter)
 
     console.log("foundPosition: " + foundPosition)
 
     if (foundPosition === -1) {
-        guessesLeft = guessesLeft -1;
+        guessesLeft = guessesLeft - 1;
         lettersGuessed.unshift(inputLetter);
-        console.log("GuessLeft: " + lettersGuessed);
+        console.log("letterGuessed: " + lettersGuessed);
     }
 
     while (foundPosition != -1) {
@@ -81,4 +98,3 @@ function guessCheck(inputLetter){
     console.log("GuessLeft: " + guessesLeft);
     return "<p>" + guessedRight + "</p>";
 }
-
