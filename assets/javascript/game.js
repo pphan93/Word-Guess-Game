1247ecfa1302c3440c126210ad9cbb12ac517cf1
Phuoc Phan <phuocphan12@gmail.com>
2019-01-30 19:03:31 -0500

    added object, everything is working as expected

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-var guessWord = "homework";
+var guessWord;
 var guessedRight = [];
 var wordLength;
 var guessesLeft;
@@ -13,77 +13,182 @@ var randomnum;
 var category;
 var win = 0;
 var loss = 0;
-var lossSound = "https://d3qhmae9zx9eb.cloudfront.net/ui/gameshow/amzn_ui_sfx_gameshow_negative_response_01.mp3"
-var winSound = "https://d3qhmae9zx9eb.cloudfront.net/ui/gameshow/amzn_ui_sfx_gameshow_positive_response_01.mp3"
-var lossImage = "https://sayingimages.com/wp-content/uploads/good-fail-meme.png"
-var correct = document.getElementById('myList');
-var countryName = [
-    "Afghanistan",
-    "Australia",
-    "Bahamas",
-    "Bulgaria",
-    "Belgium",
-    "Cambodia",
-    "Canada",
-    "China",
-    "Costa-Rica",
-    "Czech-Republic",
-    "Denmark",
-    "France",
-    "Germany",
-    "Italy",
-    "India",
-    "Japan",
-    "Korea",
-    "Liechtenstein",
-    "Malaysia",
-    "Norway",
-    "Vietnam",
-    "Thailand",
-    "Singapore"]
-var countryIso = [
-    "af",
-    "au",
-    "bs",
-    "bg",
-    "be",
-    "kh",
-    "ca",
-    "cn",
-    "cr",
-    "cz",
-    "dk",
-    "fr",
-    "de",
-    "it",
-    "in",
-    "jp",
-    "kr",
-    "li",
-    "my",
-    "no",
-    "vn",
-    "th",
-    "sg"]
-//var gameOver = false;
-
-failMessage = "YOU FAILED!";
-correctMessage = "YOU WIN";
-
-
 
 var i = 0
 
-//var hangman = {
-//    wordLength: guessWord.length,
-//    failMessage: "YOU FAILED!",
-//    correctMessage: "YOU WIN",
-//    guessesLeft: 13,
-//}
+var hangman = {
+    failMessage: "YOU FAILED!",
+    correctMessage: "YOU WIN",
+    correct: document.getElementById('myList'),
+    lossSound: "https://d3qhmae9zx9eb.cloudfront.net/ui/gameshow/amzn_ui_sfx_gameshow_negative_response_01.mp3",
+    winSound: "https://d3qhmae9zx9eb.cloudfront.net/ui/gameshow/amzn_ui_sfx_gameshow_positive_response_01.mp3",
+    lossImage: "https://sayingimages.com/wp-content/uploads/good-fail-meme.png",
+    countryName: [
+        "Afghanistan",
+        "Australia",
+        "Bahamas",
+        "Bulgaria",
+        "Belgium",
+        "Cambodia",
+        "Canada",
+        "China",
+        "Costa-Rica",
+        "Czech-Republic",
+        "Denmark",
+        "France",
+        "Germany",
+        "Italy",
+        "India",
+        "Japan",
+        "Korea",
+        "Liechtenstein",
+        "Malaysia",
+        "Norway",
+        "Vietnam",
+        "Thailand",
+        "Singapore"],
+    countryIso: [
+        "af",
+        "au",
+        "bs",
+        "bg",
+        "be",
+        "kh",
+        "ca",
+        "cn",
+        "cr",
+        "cz",
+        "dk",
+        "fr",
+        "de",
+        "it",
+        "in",
+        "jp",
+        "kr",
+        "li",
+        "my",
+        "no",
+        "vn",
+        "th",
+        "sg"],
+    initGame: function() {
+        guessesLeft = 8;
+        guessedRight = [];
+        lettersGuessIncorrect = [];
+        lettersGuessed = [];
+        this.correct.innerHTML = '';
+        document.getElementById("imageSRC").src = "assets/images/namesofcountries.jpg"
+    
+        this.getWord();
+        wordLength = guessWord.length;
+        this.wordReady(wordLength);
+    
+        var classClear = document.getElementsByClassName("clear");
+        for (var i = 0; i < classClear.length; i++) {
+            classClear[i].innerHTML = "";
+            classClear[i].classList.remove("text-danger", "text-success")
+        }
+    
+        console.log("initial: " + guessWord)
+        console.log("initial: " + wordLength);
+        //classClear.innerHTML = ""
+        // document.body.innerHTML = "";
+        //document.getElementById("wordBox").innerHTML = "";
+    },
+    getWord: function() {
+        randomnum = Math.floor(Math.random() * this.countryName.length);
+        category = this.countryIso[randomnum]
+        guessWord = this.countryName[randomnum];
+        console.log("Artist: " + guessWord + " Type: " + category)
+        guessWord = guessWord.toUpperCase();
+    },
+    wordReady: function(wordLength) {
+        //wordHolder = document.getElementById('hold');
+    
+    
+        console.log("SPace: " + wordLength)
+        lettersLeft = wordLength;
+    
+        for (i = 0; i < wordLength; i++) {
+            //correct.setAttribute('id', 'my-word');
+    
+            guess = document.createElement('li');
+            guess.setAttribute('class', 'guess');
+            if (guessWord[i] === "-") {
+                //guessedRight[i] = "-";
+                guess.innerHTML = "-";
+                lettersLeft = lettersLeft - 1;
+            }
+            else {
+                //guessedRight[i] = '_';
+                guess.innerHTML = "_";
+            }
+    
+            guessedRight.push(guess);
+            //wordHolder.appendChild(correct);
+            this.correct.appendChild(guess);
+        }
+        console.log("guess INIT: " + guess);
+        console.log(guessedRight);
+        /*else {
+            while (findSpace != -1) {
+                guessedRight[findSpace] = " ";
+                foundPosition = guessWord.indexOf(inputLetter, findSpace + 1)
+                lettersLeft = lettersLeft - 1;
+            }
+        }*/
+    },
+    resultOuput: function () {
+        if (guessesLeft === 0) {
+            this.timer();
+            loss = loss + 1;
+            var idclass = document.getElementById("status");
+            idclass.setAttribute('class', 'clear text-danger')
+            document.getElementById("lossScore").innerHTML = "<h3>" + loss + "</h3>"
+            document.getElementById("imageSRC").src = this.lossImage
+            var audio = new Audio(this.lossSound);
+            audio.play();
+            return document.getElementById("status").innerHTML = this.failMessage;
+    
+        }
+    
+        else if (lettersLeft === 0) {
+            this.timer();
+            win = win + 1;
+            var idclass = document.getElementById("status");
+            idclass.setAttribute('class', 'clear text-success')
+            document.getElementById("winScore").innerHTML = "<h3>" + win + "</h3>"
+            document.getElementById("imageSRC").src = "http://flagpedia.net/data/flags/normal/" + this.countryIso[randomnum] +".png"
+            var audio = new Audio(this.winSound);
+            audio.play();
+            return document.getElementById("status").innerHTML = this.correctMessage;
+            //document.getElementById("wordBox").innerHTML = textOutput;
+        }
+    },
+    
+    gameOver: function (guessesLeft, lettersLeft) {
+        if (guessesLeft === 0 || lettersLeft === 0) {
+            return this.resultOuput();
+        }
+    },
+    timer: function () {
+        var timeleft = 3;
+        var downloadTimer = setInterval(function () {
+            document.getElementById("countdown").innerHTML = "Restarting in " + timeleft + " seconds remaining";
+            timeleft -= 1;
+            if (timeleft <= 0) {
+                clearInterval(downloadTimer);
+                hangman.initGame();
+                //document.getElementById("countdown").innerHTML = "Finished"
+            }
+        }, 1000);
+    }
+    
+}
 
 //guessWord = guessWord.toUpperCase();
 
-initGame();
+hangman.initGame();
 
 console.log("testing: " + lettersLeft);
 
@@ -94,7 +199,7 @@ document.body.onkeyup = function (event) {
         textOutput = guessCheck(inputLetter);
         OutputStatus();
         //var inputLetter = prompt("Type in a letter to guess: ");
-        gameOver(guessesLeft, lettersLeft);
+        hangman.gameOver(guessesLeft, lettersLeft);
     }
 
 }
@@ -105,127 +210,6 @@ function OutputStatus() {
     //document.getElementById("wordBox").innerHTML = textOutput;
     document.getElementById("guessedL").innerHTML = lettersGuessIncorrect;
     document.getElementById("guessRemaining").innerHTML = "You have " + guessesLeft + " lives.";
-}
-
-
-function resultOuput() {
-    if (guessesLeft === 0) {
-        timer();
-        loss = loss + 1;
-        var idclass = document.getElementById("status");
-        idclass.setAttribute('class', 'clear text-danger')
-        document.getElementById("lossScore").innerHTML = "<h3>" + loss + "</h3>"
-        document.getElementById("imageSRC").src = lossImage
-        var audio = new Audio(lossSound);
-        audio.play();
-        return document.getElementById("status").innerHTML = failMessage;
-
-    }
-
-    else if (lettersLeft === 0) {
-        timer();
-        win = win + 1;
-        var idclass = document.getElementById("status");
-        idclass.setAttribute('class', 'clear text-success')
-        document.getElementById("winScore").innerHTML = "<h3>" + win + "</h3>"
-        document.getElementById("imageSRC").src = "http://flagpedia.net/data/flags/normal/" + countryIso[randomnum] +".png"
-        var audio = new Audio(winSound);
-        audio.play();
-        return document.getElementById("status").innerHTML = correctMessage;
-        //document.getElementById("wordBox").innerHTML = textOutput;
-    }
-}
-
-
-function gameOver(guessesLeft, lettersLeft) {
-    if (guessesLeft === 0 || lettersLeft === 0) {
-        return resultOuput();
-    }
-}
-
-
-function initGame() {
-    guessesLeft = 8;
-    guessedRight = [];
-    lettersGuessIncorrect = [];
-    lettersGuessed = [];
-    correct.innerHTML = '';
-    document.getElementById("imageSRC").src = "assets/images/namesofcountries.jpg"
-
-    getWord();
-    wordLength = guessWord.length;
-    wordReady(wordLength);
-
-    var classClear = document.getElementsByClassName("clear");
-    for (var i = 0; i < classClear.length; i++) {
-        classClear[i].innerHTML = "";
-        classClear[i].classList.remove("text-danger", "text-success")
-    }
-
-    console.log("initial: " + guessWord)
-    console.log("initial: " + wordLength);
-    //classClear.innerHTML = ""
-    // document.body.innerHTML = "";
-    //document.getElementById("wordBox").innerHTML = "";
-}
-
-function getWord() {
-    randomnum = Math.floor(Math.random() * countryName.length);
-    category = countryIso[randomnum]
-    guessWord = countryName[randomnum];
-    console.log("Artist: " + guessWord + " Type: " + category)
-    guessWord = guessWord.toUpperCase();
-}
-
-function wordReady(wordLength) {
-    //wordHolder = document.getElementById('hold');
-
-
-    console.log("SPace: " + wordLength)
-    lettersLeft = wordLength;
-
-    for (i = 0; i < wordLength; i++) {
-        //correct.setAttribute('id', 'my-word');
-
-        guess = document.createElement('li');
-        guess.setAttribute('class', 'guess');
-        if (guessWord[i] === "-") {
-            //guessedRight[i] = "-";
-            guess.innerHTML = "-";
-            lettersLeft = lettersLeft - 1;
-        }
-        else {
-            //guessedRight[i] = '_';
-            guess.innerHTML = "_";
-        }
-
-        guessedRight.push(guess);
-        //wordHolder.appendChild(correct);
-        correct.appendChild(guess);
-    }
-    console.log("guess INIT: " + guess);
-    console.log(guessedRight);
-    /*else {
-        while (findSpace != -1) {
-            guessedRight[findSpace] = " ";
-            foundPosition = guessWord.indexOf(inputLetter, findSpace + 1)
-            lettersLeft = lettersLeft - 1;
-        }
-    }*/
-}
-
-
-function timer() {
-    var timeleft = 3;
-    var downloadTimer = setInterval(function () {
-        document.getElementById("countdown").innerHTML = "Restarting in " + timeleft + " seconds remaining";
-        timeleft -= 1;
-        if (timeleft <= 0) {
-            clearInterval(downloadTimer);
-            initGame();
-            //document.getElementById("countdown").innerHTML = "Finished"
-        }
-    }, 1000);
 }
 
 
